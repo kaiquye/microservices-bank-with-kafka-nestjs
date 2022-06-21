9d5628d1fd500155a8b2e4c3841b77924985a165
kaiyque <kaiquemendesilva82@gmail.com>
2022-06-21 16:14:01 -0300

    🔐 : configurando variaveis

--- main-application/src/main.ts
+++ main-application/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 async function bootstrap() {
+  ConfigModule.forRoot({
+    isGlobal: true,
+  });
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
